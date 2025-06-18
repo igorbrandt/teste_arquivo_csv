f1673f0e056d709926e325f2bcabc08993cc627b
unknown <igorbrandt95@hotmail.com>
2025-06-17 22:22:37 -0300

    :wrench: ajustei função csv_reader em import_csv.py

--- import_csv.py
+++ import_csv.py
@@ -1,15 +1,16 @@
 import csv
 
-path_do_arquivo = "data.csv"
+file_path = "data.csv"
 
 def csv_reader(file_to_read: str) -> list[dict]:
 
     list_of_items = []
     with open(file_to_read, mode="r", encoding="utf-8") as open_file:
-        reader = csv.DictReader(open_file)
+        reader = csv.DictReader(open_file, delimiter=";")
         for line in reader:
             list_of_items.append(line)
-
     return list_of_items
-    
-print(csv_reader(path_do_arquivo))
+
+items_list: list[dict] = csv_reader(file_path)
+print(items_list)
+
